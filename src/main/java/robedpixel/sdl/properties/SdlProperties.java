93fc465b5091be1edc3303e82c0ea1e601bf6f67
RobedPixel <3531281-robedpixel@users.noreply.gitlab.com>
2025-03-12 16:44:59 +0800

    add some methods for properties module

--- src/main/java/robedpixel/sdl/properties/SdlProperties.java
+++ src/main/java/robedpixel/sdl/properties/SdlProperties.java
@@ -20,4 +20,45 @@ public class SdlProperties {
         returnObject.setValue(SdlFuncs.getGlobalProperties());
         return returnObject;
     }
+
+    /**
+     * Create a group of properties.
+     * @return Returns an ID for a new group of properties, or 0 on failure; call SdlError.getError() for more information.
+     * @throws Throwable
+     */
+    public SdlPropertiesId createProperties() throws Throwable{
+        SdlPropertiesId returnObject = new SdlPropertiesId();
+        returnObject.setValue(SdlFuncs.createProperties());
+        return returnObject;
+    }
+
+    /**
+     * Copy a group of properties.
+     * @param src The properties to copy
+     * @param dst The destination properties.
+     * @return Returns true on success or false on failure; call SdlError.getError() for more information.
+     * @throws Throwable
+     */
+    public boolean copyProperties(SdlPropertiesId src, SdlPropertiesId dst) throws Throwable {
+        return SdlFuncs.copyProperties(src.getValue(),dst.getValue());
+    }
+
+    /**
+     * Lock a group of properties.
+     * @param props The properties to lock.
+     * @return Returns true on success or false on failure; call SdlError.getError() for more information.
+     * @throws Throwable
+     */
+    public boolean lockProperties(SdlPropertiesId props)throws Throwable{
+        return SdlFuncs.lockProperties(props.getValue());
+    }
+    /**
+     * Unlock a group of properties.
+     * @param props The properties to unlock.
+     * @return Returns true on success or false on failure; call SdlError.getError() for more information.
+     * @throws Throwable
+     */
+    public void unlockProperties(SdlPropertiesId props)throws Throwable{
+        SdlFuncs.unlockProperties(props.getValue());
+    }
 }
