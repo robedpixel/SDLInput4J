ce38691800d13e6d3d153cbe42471597337ab868
RobedPixel <3531281-robedpixel@users.noreply.gitlab.com>
2025-03-23 19:32:46 +0800

    partial work done on keyboard module enums

---
+++ src/main/java/robedpixel/sdl/keyboard/SdlKeycode.java
@@ -0,0 +1,11 @@
+package robedpixel.sdl.keyboard;
+//TODO: fill with SDL3 enums
+public class SdlKeycode {
+    public static int SDLK_EXTENDED_MASK         =(1 << 29);
+    public static int SDLK_SCANCODE_MASK         = (1 << 30);
+    public static int SDL_SCANCODE_TO_KEYCODE(int x){
+        return x | SDLK_EXTENDED_MASK;
+    }
+    /**< 0 */
+    public static int SDLK_UNKNOWN                = 0x00000000;
+}
